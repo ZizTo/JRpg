7e51dd65a5464cf3d4687b0f63aec889764faabb
ZizTo <92594525+ZizTo@users.noreply.github.com>
2021-12-17 19:28:59 +0300

    v0.1.2

--- 19.cpp
+++ 19.cpp
@@ -1,4 +1,4 @@
-//ZizTo
+// ZizTo
 #include <iostream>
 #include <cmath>
 #include <iomanip>
@@ -12,7 +12,7 @@ using namespace std;
 		1 - Mag
 */
 class Hero {
-private:
+public:
 	//========DATA=========
 	string h_name;
 	int h_hp;
@@ -23,6 +23,7 @@ private:
 	int h_maxhp;
 	int h_maxdmg;
 	int h_maxmana;
+	Weapon h_weapon;
 public:
 	//========TO GET ACCESS========
 	//seter
@@ -32,6 +33,7 @@ public:
 	void setName(string name) { h_name = name; }
 	void setXp(int xp) { h_xp = xp; }
 	void setMana(int mana) { h_mana = mana; }
+	void setWeapon(Weapon weapon) { h_weapon = weapon; }
 
 	//constructor
 	Hero(string name, int hp, int dmg, int clas, int xp, int mana) {
@@ -64,6 +66,7 @@ public:
 		if (h_mana > 9) {
 			h_hp += 100;
 			h_mana -= cost;
+			HealT();
 		}
 		else {
 			Atack(enemy);
@@ -73,8 +76,9 @@ public:
 		if (h_mana > 9) {
 			h_dmg = round(h_dmg * 1.1);
 			h_mana -= cost;
+			IncrDamT();
 		}
-		else { 
+		else {
 			Atack(enemy);
 		};
 	}
@@ -85,32 +89,68 @@ public:
 		if (h_class == 1) { cout << "On the field appeared mag " << h_name << " with " << h_hp << " heal points and " << h_dmg << " damage" << endl; }
 	}
 
+	void NewTurn() { cout << h_name << " got " << h_hp << " hp, " << h_dmg << " dmg, " << h_mana << " mana!" << endl; }
+
 	void Win() {
 		cout << h_name << " WIN!";
 	}
 
-	void AtackT() { cout << h_name << " atack! He atack " << h_dmg << " damage. "; }
+	void AtackT() { cout << h_name << " atack! He atack " << h_dmg << " damage. " << endl; }
+	void IncrDamT() { cout << h_name << " increase damage! He now got " << h_dmg << " damage." << endl; }
+	void HealT() { cout << h_name << " heals! He now got " << h_hp << " hp." << endl; }
 	void GetDamage() { cout << h_name << " have " << h_hp << " heal points. "; }
 
 	//=======OTHER=======
 	void start() { h_hp = h_maxhp; h_dmg = h_maxdmg; h_mana = h_maxmana; }
 
 	void upgrade(int upgr) {
-		switch (upgr)
-		{
-		case 1:
-			h_maxhp += 50;
-			break;
-		case 2:
-			h_maxdmg += 5;
-			break;
-		case 3:
-			h_maxmana += 10;
-			break;
-		default:
-			break;
+		if (h_xp >= 5) {
+			switch (upgr)
+			{
+			case 1:
+				h_maxhp += 50;
+				break;
+			case 2:
+				h_maxdmg += 5;
+				break;
+			case 3:
+				h_maxmana += 10;
+				break;
+			default:
+				break;
+			}
 		}
 	}
+};
+
+class Weapon {
+protected:
+	string w_name;
+	bool w_equiped;
+	int w_class;
+	int w_dmg;
+public:
+	Weapon(string name = "-", bool equiped = 0, int clas = 0, int dmg = 0):
+		w_name{ name }, w_equiped { equiped }, w_class{ clas }, w_dmg{dmg}
+	{}
+	void setName(string name) { w_name = name; }
+	void setClass(int clas) { w_class = clas; }
+	void setEquiped(int equiped) { w_equiped = equiped; }
+
+	string getName() { return w_name; }
+	int getClass() { return w_class; }
+	bool getEquiped() { return w_equiped; }
+};
+
+class WeaponWithEffect {
+private:
+	int w_hp;
+	int w_heal;
+	int w_mana;
+public:
+	WeaponWithEffect(string name = "-", bool equiped = 0, int clas = 0, int dmg = 0, int hp = 0, int heal = 0, int mana = 0) :
+		Weapon(name, equiped, clas, dmg), w_hp {hp}, w_heal {heal}, w_mana {mana}
+	{}
 };
 
 int main() {
@@ -118,10 +158,11 @@ int main() {
 	string inname; int inclas; int dmg; int hp; int mana;
 	cout << "Name your person and choose your class (0 - knight, 1 - mag)" << endl;
 	cin >> inname >> inclas;
+	cout << endl;
 	if (inclas == 0)
 	{
 		hp = 500; dmg = 20; mana = 10;
-	} 
+	}
 	else if (inclas == 1) {
 		hp = 300; dmg = 10; mana = 100;
 	}
@@ -143,7 +184,7 @@ int main() {
 		while (true) {
 			//New turn
 			//Player atack
-			cout << "Your turn! What you gona do? (0 - attack, 1 - heal (100hp, 10 mana), 2 - Demage Increase (+10%, 10 mana))" << endl;
+			cout << "Your turn! What you gona do? (0 - attack, 1 - heal (100hp, 10 mana), 2 - Damage Increase (+10%, 10 mana))" << endl;
 			int button;
 			cin >> button;
 
@@ -161,8 +202,6 @@ int main() {
 			default:
 				break;
 			}
-
-			cout << endl;
 
 			srand(time(0));
 			//Enemy atack
@@ -182,13 +221,16 @@ int main() {
 			}
 
 			//endturn
+			cout << endl << endl;
 			if (player.getHp() <= 0) { enemy.Win(); player.setXp(player.getXp() + enemy.getXp()); break; }
 			if (enemy.getHp() <= 0) { player.Win(); break; }
-			Sleep(6000);
-			cout << endl << endl;
+			player.NewTurn();
+			enemy.NewTurn();
+			Sleep(5000);
+			cout << endl;
 		}
 		//Upgrade
-		cout << "you can upgrade 1 - max hp, 2 - dmg, 3 - mana, 0 - stop upgrade. Cost 5 xp. You have: " << player.getXp() << "xp." << endl;
+		cout << endl << "you can upgrade 1 - max hp, 2 - dmg, 3 - mana, 0 - stop upgrade. Cost 5 xp. You have: " << player.getXp() << "xp." << endl;
 		int upgr = 1;
 		cin >> upgr;
 		while (upgr != 0) {
@@ -196,6 +238,5 @@ int main() {
 			cin >> upgr;
 		}
 	}
-	//Game
-	
+
 }
